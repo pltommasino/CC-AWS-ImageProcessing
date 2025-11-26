178048532a2f0839980fb6dacc3217f4de2b05bd
pltommasino <pltommasino@gmail.com>
2025-11-26 17:49:07 +0100

    Final version of Lambda for API use

---
+++ lambda/Lambda4API.py
@@ -0,0 +1,50 @@
+import json
+import boto3
+
+s3 = boto3.client("s3")
+
+def cors_headers():
+    return {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*", 
+        "Access-Control-Allow-Headers": "Authorization,Content-Type",
+        "Access-Control-Allow-Methods": "GET,OPTIONS",
+    }
+
+
+def lambda_handler(event, context):
+    bucket_name = event["pathParameters"]["bucket"]
+    file_name = event["queryStringParameters"]["file"]
+
+    # Validate extension
+    if "." not in file_name:
+        return {
+            "statusCode": 400,
+            "headers": cors_headers(),
+            "body": json.dumps(
+                {"error": "File name must have an extension (.jpg, .jpeg, .png)"}
+            ),
+        }
+
+    # Validate right extension
+    ext = file_name.rsplit(".", 1)[-1].lower()
+
+    if ext in ("jpg", "jpeg"):
+        content_type = "image/jpeg"
+    elif ext == "png":
+        content_type = "image/png"
+    else:
+        return {
+            "statusCode": 400,
+            "headers": cors_headers(),
+            "body": json.dumps({"error": "Only JPG, JPEG and PNG are allowed."}),
+        }
+
+    #Generate presigned URL
+    URL = s3.generate_presigned_post(Bucket=bucket_name, Key=file_name, Fields=None, Conditions=None, ExpiresIn=600)
+
+    return {
+        "statusCode": 200,
+        "headers": cors_headers(),
+        "body": json.dumps({"URL": URL}),
+    }
