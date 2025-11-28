5a2d72c76896fba245962c402229cfdfed174815
pltommasino <pltommasino@gmail.com>
2025-11-28 13:22:53 +0100

    Final version with the option to download

--- lambda/Lambda4API.py
+++ lambda/Lambda4API.py
@@ -15,36 +15,41 @@ def cors_headers():
 def lambda_handler(event, context):
     bucket_name = event["pathParameters"]["bucket"]
     file_name = event["queryStringParameters"]["file"]
+    mode = event["queryStringParameters"]["mode"]
 
-    # Validate extension
-    if "." not in file_name:
-        return {
-            "statusCode": 400,
-            "headers": cors_headers(),
-            "body": json.dumps(
-                {"error": "File name must have an extension (.jpg, .jpeg, .png)"}
-            ),
-        }
+    if mode == "upload":
 
-    # Validate right extension
-    ext = file_name.rsplit(".", 1)[-1].lower()
+        # Validate extension
+        if "." not in file_name:
+            return {
+                "statusCode": 400,
+                "headers": cors_headers(),
+                "body": json.dumps(
+                    {"error": "File name must have an extension (.jpg, .jpeg, .png)"}
+                ),
+            }
 
-    if ext in ("jpg", "jpeg"):
-        content_type = "image/jpeg"
-    elif ext == "png":
-        content_type = "image/png"
-    else:
-        return {
-            "statusCode": 400,
-            "headers": cors_headers(),
-            "body": json.dumps({"error": "Only JPG, JPEG and PNG are allowed."}),
-        }
+        ext = file_name.rsplit(".", 1)[-1].lower()
 
-    #Generate presigned URL
-    URL = s3.generate_presigned_post(Bucket=bucket_name, Key=file_name, Fields=None, Conditions=None, ExpiresIn=600)
+        if ext in ("jpg", "jpeg"):
+            content_type = "image/jpeg"
+        elif ext == "png":
+            content_type = "image/png"
+        else:
+            return {
+                "statusCode": 400,
+                "headers": cors_headers(),
+                "body": json.dumps({"error": "Only JPG, JPEG and PNG are allowed."}),
+            }
+
+        URL = s3.generate_presigned_post(Bucket=bucket_name, Key=file_name, Fields=None, Conditions=None, ExpiresIn=600)
+
+    if mode == "download":
+
+        URL = s3.generate_presigned_url("get_object", Params={"Bucket":bucket_name, "Key":f'bw-{file_name}'}, ExpiresIn=600)
 
     return {
-        "statusCode": 200,
-        "headers": cors_headers(),
-        "body": json.dumps({"URL": URL}),
-    }
+            "statusCode": 200,
+            "headers": cors_headers(),
+            "body": json.dumps({"URL": URL}),
+        }
